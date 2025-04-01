f3c250a07321fe7ea02df681859696e065a2f8fd
PatrykKryger <144521722+PatrykKryger@users.noreply.github.com>
2025-04-01 14:34:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ import tkinter as tk
 from carbon_app import CarbonApp
 from pollution_map import PollutionMapPage
 
-
 class MainApp(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title("Jakość Powietrza i Mapa")
-        self.geometry("900x650")
         self.configure(bg='#ADD8E6')
 
         # Strony aplikacji
         self.frames = {}
         for F in (CarbonApp, PollutionMapPage):
             page_name = F.__name__
-            frame = F(parent=self, controller=self)
+            frame = F(self, self)
             self.frames[page_name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
@@ -25,7 +23,6 @@ class MainApp(tk.Tk):
         """ Przełączanie między stronami aplikacji """
         frame = self.frames[page_name]
         frame.tkraise()
-
 
 if __name__ == "__main__":
     app = MainApp()
