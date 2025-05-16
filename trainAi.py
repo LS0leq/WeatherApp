a822218ee093b078bc86c9cbb2beaa9310bb3973
matizolm <145062076+matizolm@users.noreply.github.com>
2025-05-16 07:58:18 +0200

    Aktualizacja strony
    Dodanie potrzebnych funkcjonalności

---
+++ trainAi.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import pickle
+
+df = pd.read_csv('ml_dataset.csv')
+df = df.dropna(subset=['t+1'])
+
+X = df[['t-6','t-5', 't-4', 't-3', 't-2', 't-1', 't']]
+y = df['t+1']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print(f'MSE: {mse:.2f}')
+
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
